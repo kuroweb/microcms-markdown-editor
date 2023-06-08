7d4fec7f02d3b1be1e3d28758884ce6e30a19f2a
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-06-09 01:09:33 +0900

    feat: #1 不要なパッケージや設定を削除する

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,10 @@ import dynamic from 'next/dynamic'
 
 // package
 import { useFieldExtension } from 'microcms-field-extension-react'
+import { MdEditor } from 'md-editor-rt'
 
 // styles
-import '@uiw/react-md-editor/markdown-editor.css'
-import '@uiw/react-markdown-preview/markdown.css'
-
-import { MdEditor } from 'md-editor-rt'
 import 'md-editor-rt/lib/style.css'
-
-// react-md-editorがSSR非対応のためdynamicインポートで対応
-const MDEditor = dynamic(() => import('@uiw/react-md-editor'), {
-  ssr: false,
-  loading: () => <div>loading...</div>,
-})
 
 const Page = () => {
   const [markdown, setMarkdown] = useState<string | undefined>()
@@ -39,7 +30,7 @@ const Page = () => {
 
   return (
     <div data-color-mode='light'>
-      <MdEditor modelValue={markdown || ''} onChange={handleChange} />
+      <MdEditor modelValue={markdown || ''} onChange={handleChange} language='en-US' />
     </div>
   )
 }
